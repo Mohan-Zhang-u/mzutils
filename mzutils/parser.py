71013d3be3677ddb8a5e41af6e2b7a92140bf968
Ubuntu <morgan.zhang@mail.utoronto.ca>
2019-02-01 00:15:45 +0000

    gtp commit

--- mzutils/parser.py
+++ mzutils/parser.py
@@ -2,11 +2,11 @@ def argparse_bool(s):
     """
     parse the string s to boolean for argparse
     :param s:
-    :return: bool
+    :return: bool or None by default
     """
     if s.lower() in ('yes', 'y', 't', 'true', 1):
         return True
     elif s.lower() in ('no', 'n', 'f', 'false', 0):
         return False
     else:
-        raise Exception("input should be a boolean!")
+        return None
