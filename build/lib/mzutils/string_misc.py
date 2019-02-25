1088548ea2864fec67aae0c8c22bd369da2d4f8b
Mohan-Zhang-u <morgan.zhang@mail.utoronto.ca>
2019-02-25 20:44:15 +0000

    gtp commit

--- build/lib/mzutils/string_misc.py
+++ build/lib/mzutils/string_misc.py
@@ -1,5 +1,6 @@
 import nltk
-
+import pickle
+import codecs
 
 def add_spaces_between_special_characters(InputText):
     """
@@ -19,3 +20,26 @@ def add_spaces_between_special_characters(InputText):
 
 def select_first_sentence(InputText, language='english'):
     return nltk.tokenize.sent_tokenize(InputText, language)[0]
+
+
+def py_serialize(filepath, itemlist):
+    """
+    serialize python objects using pickle
+    :param filepath:
+    :param itemlist:
+    :return:
+    """
+    with codecs.open(filepath, "wb+") as fp:
+        pickle.dump(itemlist, fp)
+
+
+def py_deserialize(filepath):
+    """
+
+    :param filepath:
+    :return:
+    """
+    itemlist = None
+    with codecs.open('filepath', 'rb') as fp:
+        itemlist = pickle.load(fp)
+    return itemlist
