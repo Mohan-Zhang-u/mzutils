dcd8b9ac5d97143ebfbfe839103c21ded7778701
Ubuntu <mozhang@DefaultVM0228.amjzpqzmvxgeno3f14z4kfbt3a.xx.internal.cloudapp.net>
2019-03-29 17:26:52 +0000

    gtp commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='mzutils',
-    version='0.1040',
+    version='0.1041',
     author="Mohan Zhang",
     author_email="mohan.zhang.mz@gmail.com",
     description="A Mohan Zhang's utility package",
