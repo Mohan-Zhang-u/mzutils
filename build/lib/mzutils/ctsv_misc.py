c89a5847521af119f777823a57029c83397c6d1b
Mohan-Zhang-u <morgan.zhang@mail.utoronto.ca>
2019-07-19 11:01:02 -0600

    gtp commit

--- build/lib/mzutils/ctsv_misc.py
+++ build/lib/mzutils/ctsv_misc.py
@@ -43,6 +43,42 @@ def append_tsv(file_path, rows):
             tsv_writer.writerow(row)
 
 
+def segment_large_csv(file_path, destination_path, segmentation_length, duplicate_header=False):
+    """
+    segment a large file to several smaller files to a destination.
+    If duplicate_header is True, the first line of  the original large file will be duplicated to every segmented files,
+    results in the length of segmented file = segmentation_length + 1. which also means that
+    :param file_path:
+    :param destination_path:
+    :param segmentation_length:
+    :param duplicate_header:
+    :return: how many files are segmented.
+    """
+    filename, file_extension = os.path.splitext(os.path.basename(file_path))
+    header = None
+    with codecs.open(file_path, "r", encoding="utf-8") as fp:
+        tsv_reader = csv.reader(fp)
+        if duplicate_header:
+            header = tsv_reader.__next__()
+            segmentation_length += 1
+        j = 0
+        while True:
+            i = 0
+            j += 1
+            current_filepath = os.path.join(destination_path, filename + str(j) + file_extension)
+            with codecs.open(current_filepath, "w+", encoding="utf-8") as fp:
+                csv_writer = csv.writer(fp)
+                if duplicate_header:
+                    csv_writer.writerow(header)
+                while i < segmentation_length:
+                    try:
+                        row = next(tsv_reader)
+                        csv_writer.writerow(row)
+                        i += 1
+                    except StopIteration:
+                        return j
+
+
 def segment_large_tsv(file_path, destination_path, segmentation_length, duplicate_header=False):
     """
     segment a large file to several smaller files to a destination.
--- mzutils/__init__.py
+++ mzutils/__init__.py
@@ -1,6 +1,6 @@
 from .os_misc import *
 from .parser import *
-from .tsv_misc import *
+from .ctsv_misc import *
 from .json_misc import *
 from .data_preprocessing import *
 from .numpy_misc import *
