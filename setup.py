8bbdaeca466d6af24ec507e804b62400e4ad1ce7
Mohan-Zhang-u <morgan.zhang@mail.utoronto.ca>
2019-07-24 01:15:49 -0600

    gtp commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='mzutils',
-    version='0.1068',
+    version='0.1069',
     author="Mohan Zhang",
     author_email="mohan.zhang.mz@gmail.com",
     description="A Mohan Zhang's utility package",
