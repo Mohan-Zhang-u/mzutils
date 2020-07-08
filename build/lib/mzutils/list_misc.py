6f88477a6430ca30631c27f22dc29e7450940776
Mohan-Zhang-u <morgan.zhang@mail.utoronto.ca>
2020-07-08 18:34:25 -0400

    added new function

--- build/lib/mzutils/list_misc.py
+++ build/lib/mzutils/list_misc.py
@@ -3,3 +3,14 @@ def remove_elements_from_list(lst: list, elements: list):
     remove elements from lst.
     """
     return [e for e in lst if e not in elements]
+
+
+def pad_list(lst: list, length: int, element=''):
+    """
+    pad a list to length with elements.
+    returned list will have length equal to length.
+    """
+    if len(lst) >= length:
+        return lst[:length]
+    
+    return lst + [element for _ in range(length - len(lst))]
