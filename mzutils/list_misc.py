7fc9dfa1e67bf13ea6b42037bd80ece99dac032e
Mohan-Zhang-u <morgan.zhang@mail.utoronto.ca>
2020-07-28 06:24:06 -0400

    new func

--- mzutils/list_misc.py
+++ mzutils/list_misc.py
@@ -21,3 +21,16 @@ def split_list_with_len(lst: list, length: int):
     return a list of sublists with len == length (except the last one)
     """
     return [lst[i:i + length] for i in range(0, len(lst), length)]
+
+
+def flatten(lst):
+    """
+    convert nested list to a list of elements.
+    """
+    if type(lst) == list:
+        result = []
+        for i in lst:
+            result += flatten(i)
+        return result
+    else:
+        return [lst]
