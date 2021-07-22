9c5c0159848656f7cff7a794e23ce1cd0a9ef4f5
Mohan Zhang <morgan.zhang@mail.utoronto.ca>
2021-07-22 13:53:59 -0400

    ver change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='mzutils',
-    version='0.1323',
+    version='0.1324',
     author="Mohan Zhang",
     author_email="mohan.zhang.mz@gmail.com",
     description="Mohan Zhang's toolkit",
