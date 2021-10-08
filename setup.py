0a808dec2e1533d5a0a165783168174ee4eaa73b
Mohan Zhang <morgan.zhang@mail.utoronto.ca>
2021-10-08 06:44:03 -0400

    change version setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import setuptools
+from mzutils._version import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='mzutils',
-    version='0.1329',
+    version=__version__,
     author="Mohan Zhang",
     author_email="mohan.zhang.mz@gmail.com",
     description="Mohan Zhang's toolkit",
