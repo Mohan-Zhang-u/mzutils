ab5d3f72653acf2078064de2e6c779389fe630b7
Mohan Zhang <mohanzhang@cs.toronto.edu>
2022-02-11 15:58:20 -0500

    added SeedData

--- mzutils/data_structures.py
+++ mzutils/data_structures.py
@@ -1,4 +1,8 @@
 import heapq
+import os
+import pandas as pd
+        
+from os_funcs import mkdir_p, get_things_in_loc
 
 
 class SimplePriorityQueue():
@@ -61,3 +65,65 @@ class SimplePriorityQueue():
 
     def nsmallest(self, n, key=None):
         return heapq.nsmallest(n, self.heap)
+
+
+class SeedData:
+    """
+    A dictionary that aims to average the evaluated mean_episode_return accross different random seed.
+    Also controls where to resume the experiments from.
+    """
+
+    def __init__(self, save_path, seeds, resume_from={}):
+        self.seeds = seeds
+        self.seed_data = pd.DataFrame({
+            'algo_name': pd.Series([], dtype='str'),
+            'test_reward': pd.Series([], dtype='float'),
+            'seed': pd.Series([], dtype='int'),
+        })
+        self.save_path = save_path
+        mkdir_p(save_path)
+        self.load()
+        # set experiment range
+        self.resume_from = resume_from
+        self.resume_check_passed = False
+
+    def load(self):
+        re_list = get_things_in_loc(self.save_path)
+        if not re_list:
+            print("Cannot find the a seed_data.csv at", self.save_path, "initializing a new one.")
+            self.seed_data.to_csv(os.path.join(self.save_path, 'seed_data.csv'), index=False)
+        else:
+            self.seed_data = pd.read_csv(os.path.join(self.save_path, 'seed_data.csv'))
+            print("Loaded the seed_data.csv at", self.save_path)
+    
+    def save(self):
+        self.seed_data.to_csv(os.path.join(self.save_path, 'seed_data.csv'), index=False)
+
+    def append(self, algo_name, test_reward, seed):
+        self.seed_data.loc[len(self.seed_data)] = [algo_name, test_reward, seed]
+
+    def setter(self, algo_name, test_reward, seed):
+        # average over seed makes seed==-1
+        # online makes dataset_percent==0.0
+        self.append(algo_name, test_reward, seed)
+        averaged_reward = self.seed_data.loc[(self.seed_data['algo_name'] == algo_name)]['test_reward'].mean()
+        if seed == self.seeds[-1]: # append the average, seed now set to -1
+            self.seed_data.loc[len(self.seed_data)] = [algo_name, averaged_reward, -1]
+        self.save()
+        return averaged_reward
+
+    def resume_checker(self, current_positions):
+        """
+        current_positions has the same shape as self.resume_from
+        return True if the current loop still need to be skipped.
+        """
+        if self.resume_check_passed is True: # checker has already passed.
+            return True
+
+        if not self.resume_from:
+            self.resume_check_passed = True
+        elif all([self.resume_from[condition] is None for condition in self.resume_from]):
+            self.resume_check_passed = True
+        else:
+            self.resume_check_passed = all([self.resume_from[condition] == current_positions[condition] for condition in self.resume_from])
+        return self.resume_check_passed
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name='mzutils',
-    version='0.2006',
+    version='0.2007',
     author="Mohan Zhang",
     author_email="mohan.zhang.mz@gmail.com",
     description="Mohan Zhang's toolkit",
